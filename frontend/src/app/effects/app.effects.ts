0a7e6911e22fb0b507346d211236f20328308638
Justin Mckena <mckennaman123@gmail.com>
2020-02-03 22:10:09 -0500

    ngrx and mdb

---
+++ frontend/src/app/effects/app.effects.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType, ROOT_EFFECTS_INIT } from '@ngrx/effects';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { ExampleApiService } from '../services/example-api.service';
+import { Router } from '@angular/router';
+import { getExamples } from '../actions/home.action';
+import { EMPTY } from 'rxjs';
+
+@Injectable()
+export class AppEffects {
+  constructor(private actions$: Actions, private exampleApiService: ExampleApiService, private router: Router) {}
+
+  getExamples$ = createEffect(() => this.actions$.pipe(
+    ofType(getExamples),
+      mergeMap(() => this.exampleApiService.getExamples()
+        .pipe(
+          map(examples => ({ type: '[Home] Examples Loaded Success', examples })),
+          catchError((error) => {
+            console.log(error);
+            return EMPTY;
+          }))
+        ))
+    );
+}
